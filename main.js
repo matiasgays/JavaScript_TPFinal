c75d680f0f3f5703a84d8ad6b6465282876385ed
matiasgays <66641926+matiasgays@users.noreply.github.com>
2021-12-12 16:24:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,115 +1,96 @@
 // Simulacion de tienda virtual mediante prompt
 
-let vehiculo;
-let modelo;
-let producto;
-
-let vehiculo1;
-let modelo1;
-let producto1;
-
-let vehiculo2;
-let modelo2;
-let producto2;
-
-let vehiculo3;
-let modelo3;
-let producto3;
-
-function mensajeBienvenida(){
-    return prompt(`Bienvenidos a la tienda online de Weskan. Seleccione un numero:
-                        1- Comprar (Puede comprar hasta 3 productos)
-                        2- Ver Carrito
-                        3- Finzalizar Compra`);
-}
-
-function comprar(a, b, c){
-    let salir = 0;
-    while(salir == 0){
-        let opcion = prompt(`Seleccione un numero:
-                            1- ${a}
-                            2- ${b}
-                            3- ${c}`)
-        switch(opcion){
-            case '1':
-                salir = 1;
-                return a;
-                break;
-            case '2':
-                salir = 1;
-                return b;
-                break;
-            case '3':
-                salir = 1;
-                return c;
-                break;
-            default:
-                break
-        }
+class Producto{
+    constructor(sku, vehiculo, marca, autoparte, stock, precio){
+        this.sku = sku;
+        this.vehiculo = vehiculo;
+        this.marca = marca;
+        this.autoparte = autoparte;
+        this.stock = stock;
+        this.precio = precio;
     }
 }
 
-function mostrarProducto(num){
-    switch(num){
-        case 1:
-            return console.log(`Producto: ${producto1} de ${modelo1} para ${vehiculo1}`);
+const productoA = new Producto(1,"Auto", "Mercedes Benz", "Asiento de valvula", 100, 1500);
+const productoB = new Producto(2,"Auto", "Mercedes Benz", "Guia de valvula", 100, 1000);
+const productoC = new Producto(3,"Auto", "Mercedes Benz", "Valvula", 100, 2500);
+const productoD = new Producto(4,"Auto", "BMW", "Asiento de valvula", 100, 1800);
+const productoE = new Producto(5,"Auto", "BMW", "Guia de valvula", 100, 900);
+const productoF = new Producto(6,"Auto", "BMW", "Valvula", 100, 2000);
+const productoG = new Producto(7,"Moto", "Yamaha", "Asiento de valvula", 100, 600);
+const productoH = new Producto(8,"Moto", "Yamaha", "Guia de valvula", 100, 500);
+const productoI = new Producto(9,"Moto", "Yamaha", "Valvula", 100, 800);
+const productoJ = new Producto(10,"Moto", "Kawasaki", "Asiento de valvula", 100, 700);
+const productoK = new Producto(11,"Moto", "Kawasaki", "Guia de valvula", 100, 600);
+const productoL = new Producto(12,"Moto", "Kawasaki", "Valvula", 100, 900);
 
-        case 2:
-            return console.log(`Producto: ${producto2} de ${modelo2} para ${vehiculo2}`);
+let productos = [];
+const listaProductos = [productoA,productoB,productoC,productoD,productoE,productoF,productoG,productoH,productoI,productoJ,productoK,productoL];
 
-        case 3:
-            return console.log(`Producto: ${producto3} de ${modelo3} para ${vehiculo3}`);
+const opciones = ['Comprar','Ver Productos','Finzalizar Compra'];
+const vehiculos = ['Auto', 'Moto'];
+const marcasAutos = ['BMW','Mercedes Benz'];
+const marcasMotos = ['Yamaha','Kawasaki'];
+const marcas = [marcasAutos, marcasMotos];
+const autopartes = ['Asiento de valvula','Guia de valvula', 'Valvula'];
 
-        default:
-            return;
-    }
+function devolverProducto(a, b, c){
+    let filtroA = listaProductos.filter(producto => producto.vehiculo == a);
+    let filtroB = filtroA.filter(producto => producto.marca == b);
+    let filtroC = filtroB.filter(producto => producto.autoparte == c);
+    return filtroC;
 }
 
-function asignarProducto(posicion){
-    switch(posicion){
-        case 0:
-            vehiculo1 = vehiculo;
-            modelo1 = modelo;
-            producto1 = producto;
-            break;
+function devolverNombre(num, lista){
+    return lista[num-1];
+}
 
-        case 1:
-            vehiculo2 = vehiculo;
-            modelo2 = modelo;
-            producto2 = producto;
-            break;
+function comprar(){
+    let numeroA = parseInt(prompt(`Seleccione:
+                                            1- ${vehiculos[0]}
+                                            2- ${vehiculos[1]}`));
+    let vehiculoElegido = devolverNombre(numeroA, vehiculos);
+    console.log(vehiculoElegido);
+    let numeroB = parseInt(prompt(`Seleccione:
+                                    1- ${marcas[numeroA-1][0]}
+                                    2- ${marcas[numeroA-1][1]}`));
+    let marcaElegida = devolverNombre(numeroB,marcas[numeroA-1]);
+    console.log(marcaElegida);
+    let numeroC = parseInt(prompt(`Seleccione:
+                                            1- ${autopartes[0]}
+                                            2- ${autopartes[1]}
+                                            3- ${autopartes[2]}`));
+    let autoparteElegida = devolverNombre(numeroC, autopartes);
+    console.log(autoparteElegida);
+    let productoElegido = devolverProducto(vehiculoElegido, marcaElegida, autoparteElegida);
+    console.log(productoElegido);
+    return productoElegido;
+    
+}
 
-        case 2:
-            vehiculo3 = vehiculo;
-            modelo3 = modelo;
-            producto3 = producto;
-            break;
+function mostrarProductos(){
+    for(let i = 0; i < cantidad; i++){
+        console.log(productos[i]);
     }
     return;
 }
 
-let i = 0;
 let salir = 0;
-while((i <=2) && salir == 0){
-    switch(mensajeBienvenida()){
+let cantidad = 0;
+while((cantidad <= 2) && (salir == 0)){
+    let opcion = prompt(`Bienvenido a la tienda online. Seleccione un numero (max 3 items):
+                            1- ${opciones[0]}
+                            2- ${opciones[1]}
+                            3- ${opciones[2]}`);
+    console.log(opcion);
+    switch(opcion){
         case '1':
-            vehiculo = comprar("Auto", "Moto","-");
-            if(vehiculo == 'Auto'){
-                modelo = comprar("BMW", "Mercedes Benz","-");
-                producto = comprar("Asiento de valvula", "Guia de Valvula", "Valvula");
-            }
-            else{
-                modelo = comprar("Yamaha", "Kawasaki","-");
-                producto = comprar("Asiento de valvula", "Guia de Valvula", "Valvula");
-            }
-            asignarProducto(i);
-            i++;
+            productos[cantidad] = comprar();
+            cantidad ++;
             break;
-        
+
         case '2':
-            for(let j = 0; j <= i; j++){
-                mostrarProducto(j);
-            }
+            mostrarProductos();
             break;
 
         case '3':
@@ -118,6 +99,4 @@ while((i <=2) && salir == 0){
     }
 }
 
-for(let k = 0; k<= i; k++){
-    mostrarProducto(k);
-}
+mostrarProductos();
